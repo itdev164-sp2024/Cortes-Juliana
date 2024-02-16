79be58ee69936072620d34ea0e97fff86f7ff625
Juliana Cortes <julianacortes@CameraHD-.localdomain>
2024-02-16 13:29:45 -0500

    modification in src/components/layout.js

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -1,15 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
+import  propTypes  from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
 import "./layout.css"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: var(--size-content);
+  padding: var(--size-gutter);
+`
+  
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
