3a3ed50e23c98c8e40434e7f90cc687887982c5e
Juliana Cortes <julianacortes@CameraHD-.localdomain>
2024-03-08 15:05:56 -0500

    Final commit activity 4

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -3,11 +3,12 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 const BlogPost = ({data}) => {
-    const {title} = data.contentfulBlogPost;
+    const {title,body} = data.contentfulBlogPost;
 
     return (
         <Layout>
         <h1> {title}</h1>
+        <div dangerouslySetInnerHTML={{_html:body.childMarkdownRemark.html}}></div>
         </Layout>
     )
     }
@@ -20,6 +21,11 @@ const BlogPost = ({data}) => {
             contentfulBlogPost(slug:{eq: $slug}){
                 title
                 slug
+                body{
+                    childMarkdownRemark{
+                        html
+                    }
+                }
             }
         }
         `
